579d6355c37e41eb84542e8b755a00a2b2cb4862
jj-jakub <jj.jakub.jasinski@gmail.com>
2023-06-07 18:07:43 +0200

    Setup retrofit service creation

---
+++ app/src/main/java/com/jj/templateproject/data/config/AppConfiguration.kt
@@ -0,0 +1,5 @@
+package com.jj.templateproject.data.config
+
+data class AppConfiguration(
+    val baseUrl: String,
+)
--- app/src/main/java/com/jj/templateproject/di/koin/mainModule.kt
+++ app/src/main/java/com/jj/templateproject/di/koin/mainModule.kt
@@ -1,15 +1,31 @@
 package com.jj.templateproject.di.koin
 
+import com.jj.templateproject.BuildConfig
+import com.jj.templateproject.data.config.AppConfiguration
 import com.jj.templateproject.data.network.RetrofitFactory
+import com.jj.templateproject.data.network.service.TemplateService
 import com.jj.templateproject.data.text.VersionTextProvider
 import com.jj.templateproject.presentation.ui.main.MainScreenViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
+import retrofit2.Retrofit
 
 val mainModule = module {
 
+    single {
+        AppConfiguration(
+            baseUrl = BuildConfig.ServerBaseUrl,
+        )
+    }
     single { RetrofitFactory() }
+    single {
+        get<RetrofitFactory>().retrofit(
+            baseUrl = get<AppConfiguration>().baseUrl,
+        )
+    }
     single { VersionTextProvider() }
 
     viewModel { MainScreenViewModel(versionTextProvider = get()) }
+
+    single { get<Retrofit>().create(TemplateService::class.java) }
 }
