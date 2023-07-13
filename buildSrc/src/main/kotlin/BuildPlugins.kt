3727063ec1171ccdc0b3046cde1d1c26a272a8f0
jj-jakub <jj.jakub.jasinski@gmail.com>
2023-07-13 19:26:10 +0200

    Add Crashlytics

--- buildSrc/src/main/kotlin/BuildPlugins.kt
+++ buildSrc/src/main/kotlin/BuildPlugins.kt
@@ -2,4 +2,5 @@ object BuildPlugins {
     val android by lazy { "com.android.tools.build:gradle:${Versions.gradlePlugin}" }
     val kotlin by lazy { "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}" }
     val googleServices by lazy { "com.google.gms:google-services:${Versions.googleServices}" }
+    val crashlytics by lazy { "com.google.firebase:firebase-crashlytics-gradle:${Versions.googleCrashlytics}" }
 }
